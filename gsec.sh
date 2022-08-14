db7d0322d6c331e0ce8519d1125544995cfc8208
root <guilhermedasilvacunha@outlook.com>
2022-08-13 22:18:44 -0300

    GSec Tool v0.0.4

--- gsec.sh
+++ gsec.sh
@@ -5,6 +5,7 @@ echo    "[:]--------------------------[:]"
 echo    "[1] Sub-Domain Scanner       [*]"
 echo    "[2] Domain Directory Scanner [*]"
 echo    "[3] Web Scraper              [*]"
+echo    "[4] Information Gathering    [*]"
 echo    "[:]--------------------------[:]"
 read -p "[?] O que deseja fazer: " opt
 read -p "[?] Output File: " outfile
@@ -76,6 +77,96 @@ case $opt in
 				echo "$email"
 				echo "$email" >> $outfile
 		done
+		;;
+	4)
+		read -p "Iniciar Information Gathering [s/N]?" opc
+
+		if [[ $opc = "s" || $opc = "S" ]]
+		then
+		    read -p "Type <Domain.com>: " host
+		    echo ""
+		    whois=$(whois $host | grep -Eo '^[a-Z].*')
+		    echo "$whois" >> $outfile.whois
+		    echo "$whois"
+
+		    echo "--------------------------------------------------------------------------"
+		    echo "Mapeando Regsitros do Dominio..."
+		    lista=['soa','a','aaaa','ns','cname','mx','ptr','hinfo','txt']
+		    echo "Executando DNS Scan com os registros: ${lista[*]}"
+		    echo ""
+
+		    hostmaster=$(host -t 'soa' $host | grep -o "[a-Z0-9.]*$host[.a-Z0-9]*")
+		    ip=$(host -t 'a' $host | grep -o "[a-Z0-9.]*$host[.a-Z0-9]*.\|[0-9].*")
+		    ipv6=$(host -t 'aaaa' $host)
+		    ns=$(host -t 'ns' $host | grep -o "[a-Z0-9.]*.$host")
+		    cname=$(host -t 'cname' $host)
+		    mx=$(host -t 'mx' $host | cut -d " " -f7 | cut -d "." -f1,2,3,4)
+		    ptr=$(host -t 'ptr' $host)
+		    hinfo=$(host -t 'hinfo' $host)
+		    vspf=$(host -t 'txt' $host | grep -o "[\"]v=spf[a-Z0-9] include:.*[a-Z0-9][\"]")
+
+		    let i=0
+		    for dns in $hostmaster
+		    do
+		        echo "\"hostmaster$[i=i+1]\":\"$dns\"," >> $outfile.log
+		    done
+
+		    domain=$(echo $ip | cut -d " " -f1)
+		    dnsaddr=$(echo $ip | cut -d " " -f2)
+
+		    echo "\"dns\":\"$domain\"," >> $outfile.log
+		    echo "\"ip\":\"$dnsaddr\"," >> $outfile.log
+
+		    if [[ "$ipv6" == "$host has no AAAA record" ]]
+		    then
+		        echo "\"ipv6\":\"NoIpv6\"," >> $outfile.log
+		    else
+		        ipv6=$(echo $ipv6 | cut -d " " -f5)
+		        echo "\"ipv6\":\"$ipv6\"," >> $outfile.log
+		    fi
+
+		    let i=0
+		    for name in $ns
+		    do
+		        echo "\"NameServer$[i=i+1]\":\"$name\"," >> $outfile.log
+		    done
+
+		    let i=0
+		    for mail in $mx
+		    do
+		        echo "\"MailExchange$[i=i+1]\":\"$mail\"," >> $outfile.log
+		    done
+
+
+		    if [[ "$ptr" != "$host has no PTR record" ]]
+		    then
+		        echo "\"Ptr\":\"$ptr\"," >> $outfile.log
+		    fi
+
+		    if [[ "$hinfo" != "$host has no HINFO record" ]]
+		    then
+		        hinfo=$(host -t 'hinfo' $host | sed 's/\"//g' | cut -d " " -f4-)
+		        echo "\"Hinfo\":\"$hinfo\"," >> $outfile.log
+		    fi
+
+
+		    echo "\"VSFP\":$vspf," >> $outfile.log
+		    host -t 'txt' $host | grep -o "[\"].*[\"]" | sed 's/^/"Text":/g' | sed 's/$/,/g' >> $outfile.log
+
+		    let i=0
+		    for nameserver in $ns
+		    do
+		        dns=$(host -l -a $host $nameserver | grep -o  "[a-z0-9]*[.]$host" | sort -u | sed 's/^/"/g' | sed 's/$/",/g')
+		        for address in $dns
+		        do
+		            echo "\"dns$[i=i+1]\":$address" >> $outfile.log
+		        done
+		    done
+
+		    jparser=$(cat $outfile.log | sed '1i{' | sed '$a}')
+		    echo $jparser > $outfile.log
+		    cat $outfile.log
+		fi
 		;;
 	*)
 		;;
